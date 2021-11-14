ed8cb02143ae6ed979f53fd1957e0b00147a97ce
TetianaBu <tetianataker@gmail.com>
2021-11-14 17:11:14 +0100

    add main functionality

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
+import {
+  ProductItemStyles,
+  ItemImageWrapper,
+  ProductItemButton
+} from './styles/ProductItemStyles';
+import emptyCart from '../assets/icons/emptyWhiteCart.svg';
+import { CurrencyContext } from './CurrencyContext';
+import { CartContext } from './CartContext';
+import { getCurrencySymbol } from '../lib/currency';
 
 export class ProductItem extends Component {
+  getPriceByCurrency = (currency) => {
+    const { prices } = this.props.product;
+    return prices.find((price) => price.currency === currency)?.amount;
+  };
   render() {
-    return <div>product item</div>;
+    const {
+      name,
+      gallery: [firstImage]
+    } = this.props.product;
+    return (
+      <CartContext.Consumer>
+        {({ addItemToCart }) => (
+          <ProductItemStyles>
+            <ItemImageWrapper>
+              <img src={firstImage} className="item-img" alt={name} />
+              <ProductItemButton
+                onClick={() => addItemToCart(this.props.product)}
+              >
+                <img src={emptyCart} alt="cart" className="cart-svg" />
+              </ProductItemButton>
+            </ItemImageWrapper>
+            <p className="product-title">{name}</p>
+            <CurrencyContext.Consumer>
+              {({ currency }) => (
+                <p className="product-price">
+                  {this.getPriceByCurrency(currency)}{' '}
+                  {getCurrencySymbol(currency)}
+                </p>
+              )}
+            </CurrencyContext.Consumer>
+          </ProductItemStyles>
+        )}
+      </CartContext.Consumer>
+    );
   }
 }
 
