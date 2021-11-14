2ca7b7c8a94c539de3229d2ca72877f09b6c12f6
TetianaBu <tetianataker@gmail.com>
2021-11-14 17:11:49 +0100

    currency styles change

--- src/components/styles/NavStyles.js
+++ src/components/styles/NavStyles.js
@@ -1,44 +1,42 @@
 import styled from 'styled-components';
- 
+
 const NavStyles = styled.header`
- margin: 0;
- padding: 0;
- justify-content: space-between;
- align-items: center;
- align-content: center;
- display: flex;
- flex-direction: row;
- text-transform: uppercase;
- height: 80px;
- ul {
-   display: flex;
-   flex-direction: row;
-   list-style: none;
-   margin: 0;
-   padding: 0;
-   width: 134px;
- }
- div {
-   width: 134px;
-   display: flex;
-   justify-content: flex-end;
-   align-items: center;
-}
- li {
-   padding-bottom: 1rem;
- }
- li:hover {
-   border-bottom: 1px solid var(--primary-light);
- }
- a:hover {
-   color: var(--primary-light);
- 
- }
- a {
-   text-decoration: none;
-   padding: 1rem;
-   color: var(--primary-dark);
- }
+  margin: 0;
+  padding: 0 2rem 0 2rem;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  flex-direction: row;
+  text-transform: uppercase;
+  height: 80px;
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: 134px;
+  }
+  div {
+    width: 134px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  li {
+    padding-bottom: 1rem;
+  }
+  a {
+    text-decoration: none;
+    padding: 2rem;
+    color: var(--primary-dark);
+  }
+  .nav-link {
+    color: var(--primary-light);
+    text-decoration: var(--primary-light);
+  }
 `;
- 
+
 export default NavStyles;
