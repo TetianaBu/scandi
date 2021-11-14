608222cba993bba00fc3d7cf98d3abc406c7e906
TetianaBu <tetianataker@gmail.com>
2021-11-14 15:00:27 +0100

    currency styles basics

---
+++ src/components/styles/CurrencySwitcherStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const CurrenctSwitcherStyles = styled.div`
+  position: fixed;
+  flex-direction: column;
+  z-index: 1;
+  background-color: white;
+  top: 10%;
+  width: 114px;
+  font-size: 18px;
+
+  input[type='radio'] {
+  }
+`;
