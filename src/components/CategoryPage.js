40355998162ca8bbfb6172a0def7ceb39c9b91be
TetianaBu <tetianataker@gmail.com>
2021-11-14 15:01:37 +0100

    category page

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,47 +1,39 @@
 import { Component } from 'react';
 import { Query } from '@apollo/client/react/components';
-import { Context } from './Context';
+import { CartContext} from './CartContext';
+import ProductItem from './ProductItem';
 import CategoryPageStyles from './styles/CategoryPageStyles';
 import ALL_PRODUCTS_BY_CATEGORY from '../apollo/allProductsByCategoryQuery';
 
 class CategoryPage extends Component {
-  static contextType = Context;
-
+  static contextType = CartContext;
   render() {
     const { category } = this.props;
-    console.log(Context);
-    console.log(this.props);
-    console.log({category})
     return (
       <CategoryPageStyles>
         <h1>{category}</h1>
-        <div className="listing-page-product-container">
-          <Query
-            query={ALL_PRODUCTS_BY_CATEGORY}
-            variables={{ category: category }}
-          >
-            {({ loading, data, error }) => {
-              console.log({loading, data, error})
-              if (loading) return 'loading...';
-              if (error) return 'error...';
-              if (data) {
-                if (!data.category) {
-                  return '/404';
-                }
-                const { products } = data.category;
-                return (
-                  <>
-                    {products.map((product) => (
-                      <div key={product.name} className="listing-page-product">
-                        {product.name}
-                      </div>
-                    ))}
-                  </>
-                );
+        <Query
+          query={ALL_PRODUCTS_BY_CATEGORY}
+          variables={{ category: category }}
+        >
+          {({ loading, data, error }) => {
+            if (loading) return 'loading...';
+            if (error) return 'error...';
+            if (data) {
+              if (!data.category) {
+                return '/404';
               }
-            }}
-          </Query>
-        </div>
+              const { products } = data.category;
+              return (
+                <div className="category-page-inner-grid">
+                  {products.map((product) => (
+                    <ProductItem product={product} key={product.id} />
+                  ))}
+                </div>
+              );
+            }
+          }}
+        </Query>
       </CategoryPageStyles>
     );
   }
