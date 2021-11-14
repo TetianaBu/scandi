24bb9e0a942e450dbf28d7195c973076bc329ab3
TetianaBu <tetianataker@gmail.com>
2021-11-14 17:09:38 +0100

    basic cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import CartStyles from './styles/CartStyles';
+import { CurrencyContext } from './CurrencyContext';
+import { CartContext } from './CartContext';
 
 class Cart extends React.Component {
   render() {
     return (
-      <CartStyles>
-        cart
-      </CartStyles>
-    )
+      <CartContext.Consumer>
+        {({ itemsAddedToCart }) => (
+          <CartStyles>
+            {itemsAddedToCart.map((item, index) => (
+              <p key={index}>{item.product.name}</p>
+            ))}
+          </CartStyles>
+        )}
+      </CartContext.Consumer>
+    );
   }
 }
 
