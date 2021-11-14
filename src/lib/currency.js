1a0e38aed6f62d5c79ec7f4b51b5a78dff5bb561
TetianaBu <tetianataker@gmail.com>
2021-11-14 17:09:56 +0100

    currency manipulations

---
+++ src/lib/currency.js
@@ -0,0 +1,13 @@
+const currencySymbols = {
+  USD: { symbol: '$', name: '$ USD' },
+  GBP: { symbol: '£', name: '£ GBP' },
+  AUD: { symbol: '$ AUD', name: '$ AUD' },
+  JPY: { symbol: '¥', name: '¥ JPY' },
+  RUB: { symbol: '₽', name: '₽ RUB' }
+};
+
+export const getCurrencySymbol = (currency) =>
+  currencySymbols[currency]?.symbol ?? currency;
+
+export const getCurrencyName = (currency) =>
+  currencySymbols[currency]?.name ?? currency;
