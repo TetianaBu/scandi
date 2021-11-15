2ec7e6317e31013100da10ac5cb4a39a3c72c2ce
TetianaBu <tetianataker@gmail.com>
2021-11-15 21:48:33 +0100

    carousel start

---
+++ src/components/styles/CartCarouselStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const CartCarouselStyles = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 100%;
+  height: 185px;
+  padding: 0 0.5rem;
+  overflow: scroll;
+`;
+export default CartCarouselStyles;
