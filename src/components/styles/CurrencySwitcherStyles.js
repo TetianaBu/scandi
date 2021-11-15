bb7d165108d273bbad3f5a0f2ac5cc2e8ce817b5
TetianaBu <tetianataker@gmail.com>
2021-11-15 21:48:19 +0100

    currency switcher styles

--- src/components/styles/CurrencySwitcherStyles.js
+++ src/components/styles/CurrencySwitcherStyles.js
@@ -8,7 +8,22 @@ export const CurrenctSwitcherStyles = styled.div`
   top: 10%;
   width: 114px;
   font-size: 18px;
-
+  font-weight: 500;
+  line-height: 28.8px;
   input[type='radio'] {
+  }
+`;
+
+export const CurrenciesBtn = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border: none;
+  font-size: 18px;
+  line-height: 28.8px;
+  font-weight: 500;
+  width: 80px;
+  img {
+    padding-left: 0.4rem;
   }
 `;
