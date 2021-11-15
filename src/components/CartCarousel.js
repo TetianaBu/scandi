167f1f0a2a6117c9b9c304e90e2cfb112a49eeae
TetianaBu <tetianataker@gmail.com>
2021-11-15 21:48:48 +0100

    carousel start

---
+++ src/components/CartCarousel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CartCarouselStyles from './styles/CartCarouselStyles'
+class CartCarousel extends React.Component {
+  render() {
+    return (
+      <CartCarouselStyles>
+        {([...this.props.children]).map((img, index) => (
+          <img src={img} key={index} alt="item img" />
+        ))}
+      </CartCarouselStyles>
+    );
+  }
+}
+
+export default CartCarousel;
