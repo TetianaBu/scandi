f04cd0206f90fb974acf8959cbb364b0a5748cd8
TetianaBu <tetianataker@gmail.com>
2021-11-16 09:33:24 +0100

    base func

--- src/components/ProductDescriptionPage.js
+++ src/components/ProductDescriptionPage.js
@@ -2,30 +2,65 @@ import React from 'react';
 import ProductDescriptionPageStyles from './styles/ProductDescriptionPageStyles';
 import { Query } from '@apollo/client/react/components';
 import PRODUCT_DETAILED_DESCRIPTION from '../apollo/productDetailedDescriptionQuery';
+import { CurrencyContext } from './CurrencyContext';
+import { CartContext } from './CartContext';
+import { getCurrencySymbol } from '../lib/currency';
+import Size from './Size';
+import ProductDescriptionCarousel from './ProductDescriptionCarouselStyles'
 
 class ProductDescriptionPage extends React.Component {
   render() {
     const { id } = this.props;
     return (
-      <ProductDescriptionPageStyles>
-        <Query query={PRODUCT_DETAILED_DESCRIPTION} variables={{ productID: id }}>
-          {({ loading, data, error }) => {
-            if (loading) return 'loading...';
-            if (error) return 'error';
-            if (data) {
-              if (!data.product) {
-                return 'Redirect';
-              }
-              const { name } = data.product;
-              return (
-                <div>
-                    <div key={id}>{name}</div>
-                </div>
-              );
+      <Query query={PRODUCT_DETAILED_DESCRIPTION} variables={{ productID: id }}>
+        {({ loading, data, error }) => {
+          if (loading) return 'loading...';
+          if (error) return 'error';
+          if (data) {
+            if (!data.product) {
+              return 'Redirect';
             }
-          }}
-        </Query>
-      </ProductDescriptionPageStyles>
+            const { name, brand, description, prices, gallery } = data.product;
+            return (
+              <ProductDescriptionPageStyles>
+                <ProductDescriptionCarousel>{gallery}</ProductDescriptionCarousel>
+                <div key={id}>
+                  <div className="titles-wrapper">
+                    <h1> {brand}</h1>
+                    <h2> {name}</h2>
+                  </div>
+                  <Size />
+                  <div className="price-wrapper">
+                    <p>Price:</p>
+                    <CurrencyContext.Consumer>
+                      {({ currency }) => (
+                        <p className="price">
+                          {getCurrencySymbol(currency)}
+                          {
+                            prices.find((price) => price.currency === currency)
+                              ?.amount
+                          }
+                        </p>
+                      )}
+                    </CurrencyContext.Consumer>
+                  </div>
+                  <CartContext.Consumer>
+                    {({ addItemToCart }) => (
+                      <button
+                        className="add-btn"
+                        onClick={() => addItemToCart(data.product)}
+                      >
+                        Add to cart
+                      </button>
+                    )}
+                  </CartContext.Consumer>
+                  <p>{description}</p>
+                </div>
+              </ProductDescriptionPageStyles>
+            );
+          }
+        }}
+      </Query>
     );
   }
 }
