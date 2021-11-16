c5469df726cf78131149b4dcabb097cda80f26e3
TetianaBu <tetianataker@gmail.com>
2021-11-16 09:34:12 +0100

    add size component

---
+++ src/components/Size.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {SizeStyles} from './styles/SizeStyles'
+
+export class Size extends Component {
+  render() {
+    return (
+      <SizeStyles>
+        <button>XS</button>
+        <button>S</button>
+        <button>M</button>
+        <button>L</button>
+      </SizeStyles>
+    );
+  }
+}
+
+export default Size;
---
+++ src/components/styles/SizeStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SizeStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-direction: row;
+  align-content: center;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    background-color: white;
+    width: 63px;
+    height: 45px;
+    border: 1px solid var(--primary-dark);
+  }
+`;
