b85d8dfc2803cb8787314c5e23d7c6bf8545334d
TetianaBu <tetianataker@gmail.com>
2021-11-19 16:50:06 +0100

    btns with sizes

--- src/components/Size.js
+++ src/components/Size.js
@@ -5,10 +5,13 @@ export class Size extends Component {
   render() {
     return (
       <SizeStyles>
+        <p className="sizes-title">Size:</p>
+        <div className="sizes-btn-wrapper">
         <button disabled>XS</button>
         <button>S</button>
         <button>M</button>
         <button>L</button>
+        </div>
       </SizeStyles>
     );
   }
