f3ad410305406ab7e8a873ceed90f465be702f72
TetianaBu <tetianataker@gmail.com>
2021-11-19 16:53:12 +0100

    description with html parser

---
+++ src/components/Description.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
+export class Description extends Component {
+  render() {
+    const { text } = this.props;
+    return <div>{ReactHtmlParser(text)}</div>;
+  }
+}
+
+export default Description;
