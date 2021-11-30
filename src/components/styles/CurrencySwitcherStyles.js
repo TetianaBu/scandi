99a5dcd93231811a98d5fb9f905ae5f683dbb7dd
TetianaBu <tetianataker@gmail.com>
2021-11-30 14:36:31 +0100

    fix currencies hover

--- src/components/styles/CurrencySwitcherStyles.js
+++ src/components/styles/CurrencySwitcherStyles.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const CurrenctSwitcherStyles = styled.div`
   position: fixed;
+  padding-top: 10px;
   padding-bottom: 1rem;
   display: flex;
   flex-direction: column;
   z-index: 3;
   background-color: white;
   top: 60px;
-  right: 2.5rem;
+  right: 3rem;
   width: 114px;
   font-size: 18px;
   font-weight: 500;
@@ -16,6 +17,15 @@ export const CurrenctSwitcherStyles = styled.div`
   box-shadow: 0px -1px 13px -1px rgba(196, 197, 199, 1);
   input[type='radio'] {
     clip-path: polygon(0 0);
+    margin-left: 0;
+  }
+  label {
+    padding: 5px 5px 5px 0;
+    cursor: pointer;
+  }
+  label:hover {
+    background-color: var(--primary-dark);
+    color: white;
   }
 `;
 
